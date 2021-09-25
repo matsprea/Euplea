a3026e8812ce530f47d16b132bc67d6645dbe8d1
Matteo Spreafico <matteo.spreafico@gmail.com>
2021-09-25 21:53:55 +0200

    refactor: :truck: Move stuff

---
+++ pages/_document.js
@@ -0,0 +1,77 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="apple-mobile-web-app-title"
+            content="Euplea - Viaggi nella bellazza dell'arte"
+          />
+          <meta
+            name="application-name"
+            content="Euplea - Viaggi nella bellazza dell'arte"
+          />
+          <meta name="description" content=" " />
+          <meta name="keywords" content=" " />
+          <meta name="msapplication-config" content="/browserconfig.xml" />
+          <meta name="msapplication-TileColor" content="#4fd1c5" />
+          <meta name="theme-color" content="#4fd1c5" />
+          <link rel="shortcut icon" href="/icons/favicon.ico" />
+          <link rel="manifest" href="/manifest.json" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/icons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/icons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="194x194"
+            href="/icons/favicon-194x194.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href="/icons/android-chrome-192x192.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/icons/favicon-16x16.png"
+          />
+
+          <link
+            rel="mask-icon"
+            href="/icons/safari-pinned-tab.svg"
+            color="#4fd1c5"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
