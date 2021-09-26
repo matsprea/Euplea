5c723772586310a7c35336a33450b5769adf5d94
Matteo Spreafico <matteo.spreafico@gmail.com>
2021-09-26 12:58:51 +0200

    build: :hammer: Add build target

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
 
-module.exports = withPWA({
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = withPWA({
   pwa: {
     dest: 'public',
     runtimeCaching,
     disable: process.env.NODE_ENV === 'development',
   },
   reactStrictMode: true,
+  target: process.env.BUILD_TARGET,
 })
+
+module.exports = nextConfig
