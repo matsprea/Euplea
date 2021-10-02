1a6549c2665bf288d1ce2895aaf488161a532ce3
Matteo Spreafico <matteo.spreafico@gmail.com>
2021-10-03 00:25:37 +0200

    feat: :alembic: Experiment

---
+++ pages/product/[name].tsx
@@ -0,0 +1,18 @@
+import { useRouter } from 'next/router'
+
+const Product = () => {
+  const router = useRouter()
+  const { name } = router.query
+
+  return (
+    // `name` is defined after hydrating client-side
+    name && (
+      <div>
+        <h1>{name}</h1>
+        <p>Welcome to our product page for {name}!</p>
+      </div>
+    )
+  )
+}
+
+export default Product
