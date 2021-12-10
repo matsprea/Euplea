3c08b7361cb35bbe7f08eee6734ebebd16f17f28
Matteo Spreafico <matteo.spreafico@gmail.com>
2021-12-10 17:44:46 +0100

    feat: :lipstick: Improved ititinerary steps

--- components/ItinerarySteps.tsx
+++ components/ItinerarySteps.tsx
@@ -1,13 +1,16 @@
 import {
   Box,
-  ListItem,
-  UnorderedList,
   Link,
-  OrderedList,
   Avatar,
   Collapse,
+  Heading,
+  VStack,
+  Spacer,
+  HStack,
+  Flex,
+  Tag,
+  TagLabel,
 } from '@chakra-ui/react'
-import { ImLink } from 'react-icons/im'
 import { forwardRef } from 'react'
 import { GiGreekTemple } from 'react-icons/gi'
 
@@ -16,34 +19,52 @@ export const ItinerarySteps = forwardRef<any, any>(
     return (
       <Collapse in={isOpen} animateOpacity>
         <Box p="2" marginBottom="42px">
-          <OrderedList spacing={3} ref={ref}>
+          {/* <Heading as="h2">Cultural institution</Heading> */}
+          <VStack spacing={3} ref={ref}>
             {culturalSites.map((culturalSite, index) => (
-              <ListItem key={index}>
-                <Link
-                  href={culturalSite['?culturalInstituteOrSite'].value}
-                  isExternal
-                >
-                  {culturalSite['?culturalInstituteOrSite'].value}
-                  <ImLink />
-                  Count {culturalSite['?count'].value}
-                </Link>
-                <UnorderedList spacing={3}>
-                  {culturalSite['site'].map((site, index) => (
-                    <ListItem key={index}>
-                      <Avatar
-                        bgColor="teal"
-                        icon={<GiGreekTemple color="white" fontSize="2rem" />}
-                        src={site['?sitePreview']?.value}
-                      />
-                      {site['?siteLabel'].value}
-                      {site['?siteFullAddress']?.value}
-                      {site['?siteCityName']?.value}
-                    </ListItem>
-                  ))}
-                </UnorderedList>
-              </ListItem>
+              <Box key={index} w="100%" shadow="md" padding={2}>
+                <HStack>
+                  <Link
+                    href={culturalSite['?culturalInstituteOrSite'].value}
+                    isExternal
+                  >
+                    <Heading as="h2" size="lg">
+                      {culturalSite['?culturalInstituteOrSiteLabel'].value}
+                    </Heading>
+                  </Link>
+                  <Spacer />
+                  <Tag size="lg" borderRadius="full" minWidth="100px">
+                    <TagLabel>{culturalSite['?count'].value} items</TagLabel>
+                  </Tag>
+                </HStack>
+                <Box>
+                  <VStack>
+                    {/* <Heading as="h4">Sites:</Heading> */}
+                    {/* <UnorderedList spacing={3}>*/}
+                    {culturalSite['site'].map((site, index) => (
+                      <Flex Box key={index} w="100%" padding={2}>
+                        <Avatar
+                          bgColor="teal"
+                          icon={<GiGreekTemple color="white" fontSize="2rem" />}
+                          src={site['?sitePreview']?.value}
+                          marginRight={2}
+                        />
+                        <Box>
+                          <Heading as="h3" size="md">
+                            {site['?siteLabel'].value}
+                          </Heading>
+                          {site['?siteFullAddress']?.value},{' '}
+                          {site['?siteCityName']?.value}
+                        </Box>
+                        <Spacer />
+                      </Flex>
+                    ))}
+                    {/*</UnorderedList> */}
+                  </VStack>
+                </Box>
+              </Box>
             ))}
-          </OrderedList>
+          </VStack>
         </Box>
       </Collapse>
     )
