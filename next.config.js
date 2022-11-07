49ba47fd2afc48c015cd70f6f49a5b1af1679c8f
Matteo Spreafico <matteo.spreafico@gmail.com>
2022-11-07 21:24:25 +0100

    fix: :bug: fix withPWA config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,14 @@
 // @ts-check
-const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+  })
 const { i18n } = require('./next-i18next.config')
 
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = withPWA({
-  pwa: {
-    dest: 'public',
-    runtimeCaching,
-    disable: process.env.NODE_ENV === 'development',
-  },
   reactStrictMode: true,
   target: process.env.BUILD_TARGET,
   i18n,
