98ed99731e8039bcd9dfe90a131db6b4e299afaf
Matteo Spreafico <matteo.spreafico@gmail.com>
2023-11-18 14:07:12 +0100

    chore: :arrow_up: update dependencies

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,7 @@
+/** @type {import('next-i18next').UserConfig} */
 module.exports = {
   i18n: {
-    defaultLocale: 'it',
-    locales: ['it', 'en'],
-    localeDetection: true,
+    defaultLocale: 'en',
+    locales: ['it', 'en']
   },
 }
